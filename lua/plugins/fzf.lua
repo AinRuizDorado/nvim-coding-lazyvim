e70b75faf8371fac3e1e8612a747cd8a3e7d841a
Ain Elias Ruiz Dorado <aruiz@bancocredicoop.coop>
2025-02-27 15:24:26 -0300

    grug far keymaps, fzf para que abra en ubuntu, rip telescope

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -5,6 +5,14 @@ return {
     opts.fzf_opts = opts.fzf_opts or {}
     -- Cambia la opción --no-scrollbar a false
     opts.fzf_opts["--no-scrollbar"] = false
+    -- Asegúrate de que la tabla files existe
+    opts.files = opts.files or {}
+    -- Añade las opciones de ripgrep para ignorar las carpetas especificadas
+    opts.files.rg_opts = "--color=never --files --hidden --follow -g '!.git' -g '!node_modules' -g '!bin' -g '!obj' -g '!platforms' -g '!lib' -g '!www' -g '!plugins'"
+    -- Asegúrate de que la tabla grep existe
+    opts.grep = opts.grep or {}
+    -- Añade las opciones de ripgrep para ignorar las carpetas especificadas en la búsqueda de código
+    opts.grep.rg_opts = "--color=never --line-number --column --hidden --smart-case -g '!.git' -g '!node_modules' -g '!bin' -g '!obj' -g '!platforms' -g '!lib' -g '!www' -g '!plugins'"
     return opts
   end,
 }
