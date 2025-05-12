2e384fedd27de1f8683c045b1502264619a97600
Ain Ruiz Dorado <aruiz@bancocredicoop.coop>
2025-05-12 14:07:57 -0300

    banca

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -8,11 +8,14 @@ return {
     -- Asegúrate de que la tabla files existe
     opts.files = opts.files or {}
     -- Añade las opciones de ripgrep para ignorar las carpetas especificadas
-    opts.files.rg_opts = "--color=never --files --hidden --follow -g '!.git' -g '!node_modules' -g '!bin' -g '!obj' -g '!platforms' -g '!lib' -g '!www' -g '!plugins'"
+    opts.files.rg_opts =
+      "--color=never --files --hidden --follow -g '!.git' -g '!./bancamovil-backend' -g '!bancamovil-frontend/node_modules' -g '!bin' -g '!obj' -g '!bancamovil-frontend/platforms' -g '!lib' -g '!bancamovil_frontend/www' -g '!bancamovil-frontend/plugins'"
+
     -- Asegúrate de que la tabla grep existe
     opts.grep = opts.grep or {}
     -- Añade las opciones de ripgrep para ignorar las carpetas especificadas en la búsqueda de código
-    opts.grep.rg_opts = "--color=never --line-number --column --hidden --smart-case -g '!.git' -g '!node_modules' -g '!bin' -g '!obj' -g '!platforms' -g '!lib' -g '!www' -g '!plugins'"
+    opts.grep.rg_opts =
+      "--color=never --line-number --column --hidden --smart-case -g '!.git' -g '!/.bancamovil-backend' -g '!bancamovil-frontend/node_modules' -g '!bin' -g '!obj' -g '!bancamovil-frontend/platforms' -g '!lib' -g '!bancamovil-frontend/www' -g '!bancamovil-frontend/plugins'"
     return opts
   end,
 }
