e3d4c2c0eeadc6dd95c1ff7bbc55d3f25d341a20
Ain Ruiz Dorado <ain.ruiz.dorado@gmail.com>
2025-05-13 18:56:03 -0300

    test

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -40,7 +40,7 @@ map("n", "<leader>tr", "<cmd>Neotree<CR>")
 
 vim.keymap.set({"n","x"}, "p", "<Plug>(YankyPutAfter)")
 vim.keymap.set({"n","x"}, "P", "<Plug>(YankyPutBefore)")
-vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)")
+vim.keymap.set({"n","x"}, "gp", "<Plug>(YankyGPutAfter)") -- test de diff view en diff view
 vim.keymap.set({"n","x"}, "gP", "<Plug>(YankyGPutBefore)")
 
 vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
